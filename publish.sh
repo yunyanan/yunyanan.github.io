327cf3e12b039f4091a4c883c5260bb42587d6e5
yunyanan <yunyanan1@gmail.com>
2020-02-03 17:47:31 +0800

    modify publish.sh

--- publish.sh
+++ publish.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+echo "Switch to source branch and update repo..."
+git checkout source
+git pull origin source
+if [[ $? -ne 0 ]]
+then
+    echo "The working directory is dirty."
+    exit 1
+fi
+
 if [[ $(git status -s) ]]
 then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
+    echo "Commit new posts first."
+    git add --all && git commit -m "commit post"
+    git push origin source
 fi
 
 echo "Delete old publication..."
@@ -28,3 +38,4 @@ cd public && git add --all && git commit -m "Publishing post"
 
 echo "Push to origin"
 git push origin master
+cd -
